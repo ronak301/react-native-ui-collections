b889beabc746fa2fd8c147b88137e31c172f8ee7
Ronak Kothari <ronak.kothari@phonepe.com>
2018-05-06 01:10:14 +0530

    zig zag view

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Text, View } from 'react-native';
 import React, { Component } from 'react';
 import { Constants, Svg } from 'expo';
+const { width } = Dimensions.get('window')
 
 export default class App extends React.Component {
   render() {
+    console.log("width", width)
     return (
       <View style={styles.container}>
-        <View style={{ width: 400, height: 200, backgroundColor: 'white' }}>
-        <Svg height={200} width={400}>
-          {this.renderZigZagView()}
-        </Svg>
+        <View style={{ width, height: 100, backgroundColor: 'cyan' }} />
+        <View style={{ width, height: 500, backgroundColor: 'white' }} >
+          <Svg height={10} width={width}>
+              {this.renderZigZagView()}
+          </Svg>
+          <Text>yoo</Text>
         </View>
       </View>
     );
@@ -17,19 +21,23 @@ export default class App extends React.Component {
 
   renderZigZagView = () => {
     let nodes = []
+    let result = ''
     for(var i = 0; i < 40; i++) {
-      const x1 = i * 10
-      const y1 = (i % 2 === 0) ? 0 : 10
-      const x2 = x1 + 10
-      const y2 = y1 === 0 ? 10 : 0 
+      // let points = ''
+      // const x1 = i * 10
+      // const y1 = (i % 2 === 0) ? 0 : 10
+      // const x2 = x1 + 10
+      // const y2 = y1 === 0 ? 10 : 0 
+      // result += x1 + ","
+      // result += x2 + " "
+      const point1 = `${20*i},0 ${20*i+10},10 ${20*(i+1)},0`
+      console.log("point1", point1)
       nodes.push(
-        <Svg.Line
-          x1={x1+''}
-          y1={y1+''}
-          x2={x2+''}
-          y2={y2+''}
-          stroke="green"
-          strokeWidth="2"
+        <Svg.Polygon
+          points={point1}
+          fill="cyan"
+          stroke="cyan"
+          strokeWidth="1"
         />
       )
     }
@@ -40,7 +48,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'cyan',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
