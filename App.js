ae118055d184ef4f762d19acf28d1f722a6cfc0f
Ronak Kothari <ronak.kothari@phonepe.com>
2018-05-06 01:20:38 +0530

    updating assets

--- App.js
+++ App.js
@@ -9,11 +9,11 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={{ width, height: 100, backgroundColor: 'cyan' }} />
-        <View style={{ width, height: 500, backgroundColor: 'white' }} >
+        <View style={{ width, height: 500, backgroundColor: 'gray', alignItems: 'center' }} >
           <Svg height={10} width={width}>
               {this.renderZigZagView()}
           </Svg>
-          <Text>yoo</Text>
+          <Text>I am text</Text>
         </View>
       </View>
     );
@@ -21,20 +21,11 @@ export default class App extends React.Component {
 
   renderZigZagView = () => {
     let nodes = []
-    let result = ''
     for(var i = 0; i < 40; i++) {
-      // let points = ''
-      // const x1 = i * 10
-      // const y1 = (i % 2 === 0) ? 0 : 10
-      // const x2 = x1 + 10
-      // const y2 = y1 === 0 ? 10 : 0 
-      // result += x1 + ","
-      // result += x2 + " "
-      const point1 = `${20*i},0 ${20*i+10},10 ${20*(i+1)},0`
-      console.log("point1", point1)
+      const point = `${20*i},0 ${20*i+10},10 ${20*(i+1)},0`
       nodes.push(
         <Svg.Polygon
-          points={point1}
+          points={point}
           fill="cyan"
           stroke="cyan"
           strokeWidth="1"
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ react native related ui components
 
 1 ) Zig zag view component : https://exp.host/@ronakkothari/zig-zag-view
 
+![Alt text](/assets/zigZagViewImage.png?raw=true "Zig Zag View")
+
